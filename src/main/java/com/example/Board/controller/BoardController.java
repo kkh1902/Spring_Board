a91898db1cc0022ff028a8bb2d325817a7d196bb
kkh1902 <kkh1902@naver.com>
2022-09-30 17:43:32 +0900

    main page 구성

---
+++ src/main/java/com/example/Board/controller/BoardController.java
@@ -0,0 +1,14 @@
+package com.example.Board.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardController {
+
+    @GetMapping("/")
+    public String List() {
+        return "index";
+    }
+}
